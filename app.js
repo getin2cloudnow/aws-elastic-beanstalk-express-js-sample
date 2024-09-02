2b844977917553ee80d819bffe1ac16bf4aa0ea1
Peter Griekspoor <peterg@checkpoint.com>
2024-09-02 19:19:42 +0300

    change message

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello class, today we will learn more about Firewalls'));
+app.get('/', (req, res) => res.send('Hello class, today we will learn more about CloudGuard'));
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
