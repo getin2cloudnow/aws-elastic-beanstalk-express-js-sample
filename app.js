cae325875e76a9fdd54601795903f3d29c353973
Peter Griekspoor <peterg@checkpoint.com>
2024-09-05 11:33:21 +0300

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to this Quantum Workshop! :)'));
+app.get('/', (req, res) => res.send('Welcome to your Quantum Workshop!:)'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
