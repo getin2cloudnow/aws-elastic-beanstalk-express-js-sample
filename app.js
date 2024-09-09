0f005e9caf555c8ed57603e009783bf513c5e9ac
Peter Griekspoor <peterg@checkpoint.com>
2024-09-08 20:42:05 -0500

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome folks to our special R80 Workshop!'));
+app.get('/', (req, res) => res.send('Welcome to our special R80 Workshop!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
